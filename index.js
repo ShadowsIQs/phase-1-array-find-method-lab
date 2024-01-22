15830d2b20f81c5251e2d19216c80e61ce2beb09
Ryan <griffinryan77@gmail.com>
2024-01-22 16:29:08 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function superbowlWin(record){
+    const result = record.find ( object => object.result === "W" )
+    if (result === undefined){
+        return undefined
+    }
+    return  result.year
+}
